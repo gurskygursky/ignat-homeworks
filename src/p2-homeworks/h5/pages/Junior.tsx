2a3b980b199f6d5cd659bd33bb205ec1178cc063
gurskygursky <yahorhurski@gmail.com>
2021-12-10 17:28:37 +0300

    add hw6

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HW6 from '../../h6/HW6'
 
 
 export const Junior = () => {
     return (
         <div>
-            {/*<HW6/>*/}
+            <HW6/>
             {/*<HW7/>*/}
             {/*<HW8/>*/}
             {/*<HW9/>*/}
@@ -15,7 +16,3 @@ export const Junior = () => {
         </div>
     )
 }
-
-
-// сделайте по аналогии пустые страницы джун и джун+
-// туда будут добавляться следующие дз

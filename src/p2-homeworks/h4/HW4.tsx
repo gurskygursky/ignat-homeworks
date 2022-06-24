a3a430ae5d740bcf555832fffff81ba3c33b588a
yegorgursky <yahorhurski@gmail.com>
2022-06-24 19:17:07 +0300

    edit buttons, create alternative button

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -3,6 +3,7 @@ import SuperInputText from './common/c1-SuperInputText/SuperInputText'
 import s from './HW4.module.css'
 import SuperButton from './common/c2-SuperButton/SuperButton'
 import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox'
+import AlternativeSuperButton from "./common/c2-SuperButton/AlternativeSuperButton";
 
 function HW4() {
     const [text, setText] = useState<string>('')
@@ -70,7 +71,7 @@ function HW4() {
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperInputText/>*/}
-            {/*<AlternativeSuperButton/>*/}
+            <AlternativeSuperButton onClick={showAlert} title={'Click'}/>
             {/*<AlternativeSuperCheckbox/>*/}
             <hr/>
         </div>
--- src/p2-homeworks/h4/common/c2-SuperButton/AlternativeSuperButton.tsx
+++ src/p2-homeworks/h4/common/c2-SuperButton/AlternativeSuperButton.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import style from '../c2-SuperButton/SuperButton.module.css';
 
-function AlternativeSuperButton() {
+type PropsType = {
+    title: string;
+    onClick: () => void;
+    disabled?: boolean;
+};
+function AlternativeSuperButton(props: PropsType) {
+    const { onClick, title, disabled } = props;
+    const onButtonClick = (): void => {
+        onClick();
+    };
     return (
-        <button/>
-    )
+        <button className={style.btn} onClick={onButtonClick} disabled={disabled} type="button">
+            {title}
+        </button>
+    );
 }
 
 export default AlternativeSuperButton
